fdd93a0fccef83f27228d5c45356b6d09b36db69
osilvam <oscar.silvamz@gmail.com>
2014-10-10 22:38:53 -0300

    señal suavizada

--- tarea2/include/csa.h
+++ tarea2/include/csa.h
@@ -10,11 +10,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <math.h>
 void comprobacionDatosEntrada(int argc, char** argv);
 void creacionOctave( int * pid, FILE ** octaveFd );
-void obtencionDatos( char * rutaAudio , char * ganancia, int16_t ** datosBrutos , int16_t ** datosSaturados ,int * sizeOfData);
+void obtencionDatos( char * rutaAudio , char * _ganancia, int16_t ** datosBrutos , int16_t ** datosSaturados, double ** sim_time ,int * sizeOfData);
 void protocoloDeSalida(FILE ** octaveFd, pid_t pid, int16_t ** datosBrutos, int16_t ** datosSaturados);
-void generacionGraficos(FILE ** octaveFd, int16_t ** datosBrutos, int16_t ** datosSaturados, char * offset );
-void suavizamientoDeSaturacion();
+void generacionGraficos(FILE ** octaveFd, int16_t ** datosBrutos, int16_t ** datosSaturados, int16_t ** datosSuavizados, char * offset );
+void suavizamientoDeSaturacion(FILE ** octaveFd, int16_t ** datosSuavizados , int16_t ** datosSaturados, double ** sim_time, float ganancia ,int sizeOfData);
 void reproducir(int16_t * datosBrutos, int16_t * datosSaturados, int16_t * datosSuavizados, int sizeOfData);
 #endif
--- tarea2/src/csa.c
+++ tarea2/src/csa.c
@@ -10,12 +10,13 @@ int main(int argc, char** argv){
 	int16_t * datosBrutos = NULL;
 	int16_t * datosSaturados = NULL;  
 	int16_t * datosSuavizados = NULL;
+	double * sim_time = NULL;
 
 	comprobacionDatosEntrada(argc, argv);
 	creacionOctave( &pid, &octaveFd );
-	obtencionDatos( argv[1], argv[2], &datosBrutos, &datosSaturados ,&sizeOfData); // No fue ingresado p por lo tanto no se obtendran todos los datos lo que disminuye el procesamiento
-	//suavizamientoDeSaturacion( &datosSuavizados, sizeOfData );
-	generacionGraficos(&octaveFd, &datosBrutos, &datosSaturados, argv[3]);
+	obtencionDatos( argv[1], argv[2], &datosBrutos, &datosSaturados, &sim_time ,&sizeOfData); // No fue ingresado p por lo tanto no se obtendran todos los datos lo que disminuye el procesamiento
+	suavizamientoDeSaturacion(&octaveFd, &datosSuavizados, &datosSaturados, &sim_time, atof(argv[2]), sizeOfData );
+	generacionGraficos(&octaveFd, &datosBrutos, &datosSaturados, &datosSuavizados, argv[3]);
 	if(argc == 5 && (char)*argv[4] == 'p')
 		reproducir( datosBrutos, datosSaturados, datosSuavizados, sizeOfData );
 
@@ -71,7 +72,7 @@ void creacionOctave( int * pid, FILE ** octaveFd ){
 }
 
 
-void obtencionDatos( char * rutaAudio , char * _ganancia, int16_t ** datosBrutos , int16_t ** datosSaturados ,int * sizeOfData){
+void obtencionDatos( char * rutaAudio , char * _ganancia, int16_t ** datosBrutos , int16_t ** datosSaturados, double ** sim_time ,int * sizeOfData){
 	FILE * archivoAudioRaw = fopen(rutaAudio, "rb");
 	*sizeOfData = 0;
 	int16_t valor;
@@ -85,12 +86,13 @@ void obtencionDatos( char * rutaAudio , char * _ganancia, int16_t ** datosBrutos
 
 	archivoAudioRaw = fopen(rutaAudio, "rb");
 	*datosBrutos = (int16_t *) malloc(*sizeOfData * sizeof(int16_t) );
-	*datosSaturados = (int16_t *) malloc(*sizeOfData * sizeof(int16_t) );
+	*datosSaturados = (int16_t *) malloc(*sizeOfData * sizeof(int16_t) );	
+	*sim_time = (double *) malloc(*sizeOfData * sizeof(double) );
 	i=0;
 	while(	fread(&valor,sizeof(valor),1,archivoAudioRaw) != 0   ){
 		(*datosBrutos)[i] = valor;
-		int16_t saturados = ganancia*valor;
-		(*datosSaturados)[i] = saturados/ganancia;
+		(*datosSaturados)[i] = (abs(ganancia*valor) < 0x7fff) ? valor : valor/abs(valor)*(0x7fff/ganancia);
+		(*sim_time)[i] = i/8000.0;
 		i++;
 	}
 
@@ -109,7 +111,7 @@ void protocoloDeSalida(FILE ** octaveFd, int pid, int16_t ** datosBrutos, int16_
     printf("\n");fflush(stdout);
 }
 
-void generacionGraficos(FILE ** octaveFd, int16_t ** datosBrutos, int16_t ** datosSaturados, char * _offset ){ 
+void generacionGraficos(FILE ** octaveFd, int16_t ** datosBrutos, int16_t ** datosSaturados, int16_t **datosSuavizados, char * _offset ){ 
 
 	int i;
 
@@ -135,6 +137,14 @@ void generacionGraficos(FILE ** octaveFd, int16_t ** datosBrutos, int16_t ** dat
 	}
 	fprintf(*octaveFd, " ]\n" ); 
 	fflush(*octaveFd);
+	//============================================
+	fprintf(*octaveFd, "datosSuavizados = [ ");
+	for (i= offset * SAMPLES_PER_MILLISEC; i < offset * SAMPLES_PER_MILLISEC + TOTAL_GRAPH_TIME_MILLISEC * SAMPLES_PER_MILLISEC; ++i)
+	{
+		fprintf(*octaveFd, "%" PRId16 " ", (*datosSuavizados)[i]);
+	}
+	fprintf(*octaveFd, " ]\n" ); 
+	fflush(*octaveFd);
 	//===========================================
 	fprintf(*octaveFd, "tiempo = [ ");
 	for (i = offset * SAMPLES_PER_MILLISEC; i < offset * SAMPLES_PER_MILLISEC + TOTAL_GRAPH_TIME_MILLISEC * SAMPLES_PER_MILLISEC; ++i)
@@ -144,17 +154,81 @@ void generacionGraficos(FILE ** octaveFd, int16_t ** datosBrutos, int16_t ** dat
 	fprintf(*octaveFd, " ]\n" ); fflush(*octaveFd);
 	//===========================================
 
-	fprintf(*octaveFd, "subplot(2,1,1);\n");fflush(*octaveFd);
+	fprintf(*octaveFd, "subplot(3,1,1);\n");fflush(*octaveFd);
     fprintf(*octaveFd, "plot(tiempo,datosBrutos);\n");fflush(*octaveFd);
-    fprintf(*octaveFd, "subplot(2,1,2);\n");fflush(*octaveFd);
+    fprintf(*octaveFd, "subplot(3,1,2);\n");fflush(*octaveFd);
 	fprintf(*octaveFd, "plot(tiempo,datosSaturados)\n");fflush(*octaveFd);
+	fprintf(*octaveFd, "subplot(3,1,3);\n");fflush(*octaveFd);
+	fprintf(*octaveFd, "plot(tiempo,datosSuavizados)\n");fflush(*octaveFd);
+
 }
 
 
-void suavizamientoDeSaturacion(/*  ..., int16_t ** datosSuavizados, ... */){
+void suavizamientoDeSaturacion(FILE ** octaveFd, int16_t ** datosSuavizados , int16_t ** datosSaturados, double ** sim_time, float ganancia ,int sizeOfData){
 	// Deberia consistir en dos metodos
 	// obtenerPosicionesDeSaturaciones( ... )
 	// obtenerDatosSuavizados( ..., int16_t ** datosSuavizados, ... )
+	int i, var, cont = 0;
+	*datosSuavizados = (int16_t *) malloc(sizeOfData * sizeof(int16_t) );
+
+	(*datosSuavizados)[0] = 0;
+	printf("%f\n", ganancia);
+	printf("%hd\n", (int16_t)(0x7fff/ganancia));
+
+	for(i = 1; i < sizeOfData; i++)
+	{
+		if(abs((*datosSaturados)[i]) == (int16_t)(0x7fff/ganancia))
+		{
+			if(var == 0)
+			{
+				(*datosSuavizados)[i] = (*datosSaturados)[i];
+				var = i;	
+			} 
+			cont++;
+		}
+		else if(abs((*datosSaturados)[i-1]) == (int16_t)(0x7fff/ganancia))
+		{
+			(*datosSuavizados)[i] = (*datosSaturados)[i];
+
+			fprintf(*octaveFd, "y = [ ");				
+			fprintf(*octaveFd, "%" PRId16 " ", (*datosSaturados)[var-1]);
+			fprintf(*octaveFd, "%" PRId16 " ", (*datosSaturados)[var]);
+			fprintf(*octaveFd, "%" PRId16 " ", (*datosSaturados)[var+cont-1]);
+			fprintf(*octaveFd, "%" PRId16 " ", (*datosSaturados)[var+cont]);			
+			fprintf(*octaveFd, " ];\n"); 
+			fflush(*octaveFd);
+
+			fprintf(*octaveFd, "x = [ ");				
+			fprintf(*octaveFd, "%f" , (*sim_time)[var-1]);
+			fprintf(*octaveFd, "%f" , (*sim_time)[var]);
+			fprintf(*octaveFd, "%f" , (*sim_time)[var+cont-1]);
+			fprintf(*octaveFd, "%f" , (*sim_time)[var+cont]);			
+			fprintf(*octaveFd, " ];\n"); 
+			fflush(*octaveFd);
+
+			fprintf(*octaveFd, "p = polyfit(x,y,3);\n");
+			fflush(*octaveFd);
+
+			int j;
+			for(j = 1; j < cont-1; j++)
+			{
+				fprintf(*octaveFd, "polyval(p,%f)\n", (*sim_time)[var+j]);
+				fflush(*octaveFd);
+
+				int valor_suavizado = 0;
+				//AQUI LEER A OCTAVE y guardar en valor suavizado
+
+				(*datosSuavizados)[var+j] = 0;
+			}
+			var = 0;
+			cont = 0;
+		}else
+		{
+			(*datosSuavizados)[i] = (*datosSaturados)[i];
+		}
+	}
+	printf("endfor\n");
+
 }
 
 
